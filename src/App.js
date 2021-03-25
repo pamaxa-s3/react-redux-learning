094c00f6c04d4bf4a1ac3a5afa8e792030e311ac
Strynzha Roman <johndoe@example.com>
2021-03-25 12:48:04 +0200

    edux и eact. tate, reducer, action + redux hooks useispatch и useelector

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import classes from './App.module.scss'
+import { ButtonToggle } from 'reactstrap'
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
+
+  const dispatch = useDispatch()
+  const cash = useSelector(state => state.cash)
+  console.log(cash);
+
+  const addCash = (cash) => {
+    dispatch({
+      type: 'ADD_CASH',
+      payload: cash
+    })
+  }
+  const subCash = (cash) => {
+    dispatch({
+      type: 'SUB_CASH',
+      payload: cash
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.App}>
+      <h2 className={classes.cash}>{cash}</h2>
+      <div>
+        <ButtonToggle color="primary" onClick={() => addCash(Number(prompt()))}>Полнить счёт</ButtonToggle>
+        <ButtonToggle color="danger" onClick={() => subCash(Number(prompt()))}>Снять со счёта</ButtonToggle>
+      </div>
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,16 +2,40 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const initialState = {
+  cash: 0
+}
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'ADD_CASH':
+      return {
+        ...state, cash: state.cash + action.payload
+      }
+    case 'SUB_CASH':
+      return { ...state, cash: state.cash - action.payload }
+
+    default:
+      return state
+  }
+}
+
+
+const store = createStore(reducer)
+
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
+  ,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
